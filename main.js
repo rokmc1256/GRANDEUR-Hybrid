80d1f5fb6d1ab2e3b683ad3f93b9c7b7a2794b86
rokmc1256 <skh77017@naver.com>
2023-02-16 03:49:28 +0900

    현대자동차 페이지 클론코딩 연습

---
+++ main.js
@@ -0,0 +1,83 @@
+let frame3 = document.querySelector(".frame3");
+let frame3_lowText = document.querySelector(".frame3_text");
+let frame3_icon = document.querySelector(".frame3_icon");
+let muddySpace1 = document.querySelector(".muddy_space1");
+frame3.addEventListener("mouseover", () => {
+    frame3_lowText.style.display = "none"
+    frame3_icon.style.display = "none"
+    muddySpace1.style.display="block"
+});
+frame3.addEventListener("mouseout", () => {
+    frame3_lowText.style.display = ""
+    frame3_icon.style.display = ""
+    muddySpace1.style.display=""
+});
+
+
+
+let frame4 = document.querySelector(".frame4");
+let frame4_lowText = document.querySelector(".frame4_text");
+let frame4_icon = document.querySelector(".frame4_icon");
+let muddySpace2 = document.querySelector(".muddy_space2");
+frame4.addEventListener("mouseover", () => {
+    frame4_lowText.style.display = "none"
+    frame4_icon.style.display = "none"
+    muddySpace2.style.display="block"
+});
+frame4.addEventListener("mouseout", () => {
+    frame4_lowText.style.display = ""
+    frame4_icon.style.display = ""
+    muddySpace2.style.display=""
+});
+
+
+
+let frame5 = document.querySelector(".frame5");
+let frame5_lowText = document.querySelector(".frame5_text");
+let frame5_icon = document.querySelector(".frame5_icon");
+let muddySpace3 = document.querySelector(".muddy_space3");
+frame5.addEventListener("mouseover", () => {
+    frame5_lowText.style.display = "none"
+    frame5_icon.style.display = "none"
+    muddySpace3.style.display="block"
+});
+frame5.addEventListener("mouseout", () => {
+    frame5_lowText.style.display = ""
+    frame5_icon.style.display = ""
+    muddySpace3.style.display=""
+});
+
+
+
+let frame6 = document.querySelector(".frame6");
+let frame6_lowText = document.querySelector(".frame6_text");
+let frame6_icon = document.querySelector(".frame6_icon");
+let muddySpace4 = document.querySelector(".muddy_space4");
+frame6.addEventListener("mouseover", () => {
+    frame6_lowText.style.display = "none"
+    frame6_icon.style.display = "none"
+    muddySpace4.style.display="block"
+});
+frame6.addEventListener("mouseout", () => {
+    frame6_lowText.style.display = ""
+    frame6_icon.style.display = ""
+    muddySpace4.style.display=""
+});
+
+
+
+let frame7 = document.querySelector(".frame7");
+let frame7_lowText = document.querySelector(".frame7_text");
+let frame7_icon = document.querySelector(".frame7_icon");
+let muddySpace5 = document.querySelector(".muddy_space5");
+frame7.addEventListener("mouseover", () => {
+    frame7_lowText.style.display = "none"
+    frame7_icon.style.display = "none"
+    muddySpace5.style.display="block"
+});
+frame7.addEventListener("mouseout", () => {
+    frame7_lowText.style.display = ""
+    frame7_icon.style.display = ""
+    muddySpace5.style.display=""
+});
+
